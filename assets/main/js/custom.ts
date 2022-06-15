faa0684dfd6b3562afea6809739ce202d5527ba6
razonyang <razonyang@gmail.com>
2022-06-15 14:03:16 +0800

    Add ad block detect

--- assets/main/js/custom.ts
+++ assets/main/js/custom.ts
@@ -1,1 +1,16 @@
-console.log("Message from assets/js/custom.js");
+declare global {
+    interface Window { adsbygoogle: any; }
+}
+
+setTimeout(function() {
+    console.log(window.adsbygoogle);
+    var adsbygoogle = window.adsbygoogle ? window.adsbygoogle : {};
+    if (!adsbygoogle.loaded) {
+        var d = document.createElement('div');
+        d.style = 'position: fixed; z-index: 1100; background: black; color: red; width: 100%; height: 2rem; text-align: center; padding: .25rem .5rem; top: 0; display: flex; justify-content: center; align-items: center; font-weight:bold; text-transform: uppercase;';
+        d.innerText = 'Please turn off the ad blcok.';
+        document.body.prepend(d);
+        var topAppBar = document.querySelector('.top-app-bar') as HTMLHtmlElement;
+        topAppBar.style = 'top: 2rem;';
+    }
+}, 2000);
